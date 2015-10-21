a901b719dcf4ff4c000c7ff1fe33c2816397d16e
Karthik Marupeddi <Karthik@Karthiks-MBP.attlocal.net>
2015-10-21 06:49:17 -0500

    graphs still in works

--- src/com/telos/hyd/pages/TypeOfPoliciesGraph.java
+++ src/com/telos/hyd/pages/TypeOfPoliciesGraph.java
@@ -6,18 +6,31 @@ import com.codename1.charts.renderers.DefaultRenderer;
 import com.codename1.charts.renderers.SimpleSeriesRenderer;
 import com.codename1.charts.util.ColorUtil;
 import com.codename1.charts.views.DoughnutChart;
+import com.codename1.components.InfiniteProgress;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.Log;
+import com.codename1.io.NetworkManager;
 import com.codename1.ui.*;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.LayeredLayout;
 import com.codename1.ui.util.Resources;
 import com.telos.hyd.Styles.Styles;
 import com.telos.hyd.Styles.TimeUtil;
+import com.telos.hyd.model.Charts;
+import com.telos.hyd.model.ChartsMapper;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by karthikmarupeddi on 2/9/15.
@@ -40,6 +53,9 @@ public class TypeOfPoliciesGraph {
     DoughnutChart chart;
     List<double[]> values = null;
     DefaultRenderer renderer = null;
+    Container contantContainer = null;
+    ChartComponent chartComponent = null;
+    Container chartComponentContainer = null;
 
 
     public TypeOfPoliciesGraph(Form form)
@@ -55,7 +71,7 @@ public class TypeOfPoliciesGraph {
 
     public void createPieChartForm() {
 
-        Container contantContainer = PageTemplate.getContainer(PageTemplate.setBackButton(logInForm), theme, f);
+        contantContainer = PageTemplate.getContainer(PageTemplate.setBackButton(logInForm), theme, f);
 
 
         List<String[]> titles = getStrings();
@@ -63,10 +79,11 @@ public class TypeOfPoliciesGraph {
         chart = new DoughnutChart(buildMultipleCategoryDataset("Project budget", titles, values), renderer);
 
 
-
-        ChartComponent chartComponent = new ChartComponent(chart);
+        chartComponentContainer = new Container(new BorderLayout());
+        chartComponent = new ChartComponent(chart);
         chartComponent.setZoomEnabled(true);
-        wrap("Number of Type of Policies in year 2005 and 2006", chartComponent,contantContainer).show();
+        chartComponentContainer.addComponent(BorderLayout.CENTER,chartComponent);
+        wrap("Number of Type of Policies in year 2005 and 2006", chartComponentContainer, contantContainer).show();
 
         Label yearFromLabel = new Label("Month:");
         yearFromLabel.setVisible(true);
@@ -179,13 +196,159 @@ public class TypeOfPoliciesGraph {
             @Override
             public void actionPerformed(ActionEvent evt) {
                 source.setText(text);
-                renderer.setBackgroundColor(ColorUtil.LTGRAY);
-                chart = new DoughnutChart(buildMultipleCategoryDataset("Project budget", getStrings(), values), renderer);
+                //renderer.setBackgroundColor(ColorUtil.LTGRAY);
+                //chart = new DoughnutChart(buildMultipleCategoryDataset("Project budget", getStrings(), values), renderer);
                 dialog.dispose();
+
+                final ChartsMapper chartsMapper = new ChartsMapper();
+                ArrayList values = new ArrayList<double[]>();
+
+                ConnectionRequest cr = new ConnectionRequest() {
+
+                    public Map<String, Object> totalList;
+
+                    protected void readResponse(InputStream is)
+                            throws IOException {
+
+                        JSONParser p = new JSONParser();
+                        totalList = p.parseJSON(new InputStreamReader(is));
+
+
+                    }
+
+                    /**
+                     * A callback method that's invoked on the EDT after the readResponse() method has finished,
+                     * this is the place where developers should change their Codename One user interface to
+                     * avoid race conditions that might be triggered by modifications within readResponse.
+                     * Notice this method is only invoked on a successful response and will not be invoked in case
+                     * of a failure.
+                     */
+                    @Override
+                    protected void postResponse() {
+
+                        if (totalList != null) {
+                            ArrayList<Charts> dataList = new ArrayList<Charts>();
+                            ArrayList list = (ArrayList) totalList.get("root");
+                            if (list != null) {
+
+                                for (Object object : list) {
+                                    Charts chartsValue = new Charts();
+                                    chartsMapper.readMap((Map) object, chartsValue);
+                                    dataList.add(chartsValue);
+                                }
+                            }
+
+
+                            List<Double> priYearValuesList = new ArrayList<>();
+                            List<Double> compYearValuesList = new ArrayList<>();
+                            List<String> priYearTitleValue = new ArrayList<>();
+                            List<String> compYearTitleValue = new ArrayList<>();
+                            List<Double> priPercentageYearList= new ArrayList<>();
+                            List<Double> compPercentageYearList= new ArrayList<>();
+
+
+
+
+
+
+                            for (Charts charts : dataList) {
+                                if (charts.getToYear().toString().equals("2015")) {
+                                    priYearValuesList.add(charts.getNumberOfPolicies().doubleValue());
+                                    priYearTitleValue.add(charts.getDepartment());
+                                }
+                            }
+
+                            for (Charts charts : dataList) {
+                                if (charts.getToYear().toString().equals("2014")) {
+                                    compYearValuesList.add(charts.getNumberOfPolicies().doubleValue());
+                                    compYearTitleValue.add(charts.getDepartment());
+                                }
+                            }
+
+                            percentageCal(priYearValuesList, priPercentageYearList);
+                            percentageCal(compYearValuesList, compPercentageYearList);
+
+
+
+
+                            values.add(priPercentageYearList.stream().mapToDouble(Double::doubleValue).toArray());
+                            values.add(compPercentageYearList.stream().mapToDouble(Double::doubleValue).toArray());
+
+
+
+
+//                           values.add(new double[]{17, 21, 31, 20, 11});
+//                           values.add(new double[] { 12, 21, 25, 21, 12 });
+
+                            List<String[]> titles = new ArrayList<>();
+
+
+                            titles.add(priYearTitleValue.toArray(new String[priYearTitleValue.size()]));
+                            titles.add(compYearTitleValue.toArray(new String[compYearTitleValue.size()]));
+
+                            chartComponentContainer.removeComponent(chartComponent);
+                            chart = new DoughnutChart(buildMultipleCategoryDataset("Project budget", titles, values), renderer);
+                            chartComponent = new ChartComponent(chart);
+                            chartComponentContainer.addComponent(BorderLayout.CENTER, chartComponent);
+
+                        }
+
+                    }
+
+                };
+
+                try {
+
+                    cr.setUrl("http://telosws-poplar5.rhcloud.com//getChartTypeOfPolicies");
+                } catch (Exception e) {
+                    Log.p(e.toString());
+                }
+                cr.setPost(false);
+
+                InfiniteProgress progress = new InfiniteProgress();
+                progress.setUIID("InfiniteProgress");
+                Dialog dialogProgress = progress.showInifiniteBlocking();
+                cr.setDisposeOnCompletion(dialogProgress);
+                cr.addArgument("fromYear", "2014");
+                cr.addArgument("toYear", "2015");
+                NetworkManager.getInstance().addToQueue(cr);
+
             }
         });
         return layeredContainer;
     }
+
+    private void percentageCal(List<Double> priYearValuesList, List<Double> priPercentageYearList) {
+        double total = 0;
+        double totalPercentage = 0;
+        int i =0;
+
+
+        for(double value:priYearValuesList)
+        {
+            total = total +value;
+        }
+
+        for(double value:priYearValuesList)
+        {
+
+            priPercentageYearList.add((double) Math.round((value / Math.round(total)) * 100));
+            totalPercentage = totalPercentage+ (double) Math.round((value / Math.round(total)) * 100);
+
+            if(i == priYearValuesList.size() && totalPercentage  != 100)
+            {
+                double differenceValue = priYearValuesList.get(i) + (100-totalPercentage);
+                priYearValuesList.set(i,differenceValue);
+            }
+            i++;
+        }
+    }
+
 }
+
+
+
+
+
 
 
